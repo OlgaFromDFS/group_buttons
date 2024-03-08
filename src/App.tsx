627896bec0ada94346385bb9db75451ecec038c6
Olga <oluaribalka@yandex.ru>
2024-03-08 11:57:45 +0300

    added new files

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { cn } from '@bem-react/classname';
 
-function App() {
+import { Button } from './components/Button';
+
+import './App.css';
+import './components/Button.css';
+
+export const cnGeneral = cn('General');
+export const cnGroupButtons = cn('GroupButtons');
+export const cnDarkThemeButtons = cn('DarkThemeButtons');
+
+export const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className={cnGeneral()}>
+        <div className={cnGroupButtons()}>
+          <Button theme='light' role='primary' />
+          <Button theme='light' role='primary' disabled />
+          <Button theme='light' role='secondary' />
+          <Button theme='light' role='secondary' disabled />
+        </div>
+        <div className={cnGroupButtons({ theme: 'dark' })}>
+          <Button theme='dark' role='primary' />
+          <Button theme='dark' role='primary' disabled />
+          <Button theme='dark' role='secondary' />
+          <Button theme='dark' role='secondary' disabled />
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
